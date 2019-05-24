1dba7e195fbdc681f8eb826841e8d6a939e86b07
rickale <ti.ricardo.sousa@gmail.com>
2019-05-23 23:38:44 -0300

    Exercício simples de soma de 2 números testando as possibilidades do kotlin.

--- app/src/main/java/br/com/ricardo/kotlinplayground/MainActivity.kt
+++ app/src/main/java/br/com/ricardo/kotlinplayground/MainActivity.kt
@@ -2,18 +2,39 @@ package br.com.ricardo.kotlinplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    val mensagem: String = "Olá Mundão! Meu nome é Kotlin!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text_hello.text = mensagem
+    }
+
+    fun calcular(view: View){
+
+        val valor1 : String = edit_valor1.text.toString()
+        val valor2 : String = edit_valor2.text.toString()
+
+        var resultado : Double = 0.0
+
+        if(TextUtils.isEmpty(valor1)){
+            edit_valor1.error = "Campo obrigatório!"
+
+        } else if (TextUtils.isEmpty(valor2)){
+            edit_valor2.error = "Campo obrigatório!"
+
+        } else {
+            resultado = (valor1.toDouble() + valor2.toDouble())
+            text_resultado.text = resultado.toString()
+
+        }
 
     }
 }
