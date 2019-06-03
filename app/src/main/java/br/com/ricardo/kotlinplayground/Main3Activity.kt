f1d3b1adf2e2dde36326d953ea8de8990cf47e6e
rickale <ti.ricardo.sousa@gmail.com>
2019-06-02 22:07:26 -0300

    Playing with xml

--- app/src/main/java/br/com/ricardo/kotlinplayground/Main3Activity.kt
+++ app/src/main/java/br/com/ricardo/kotlinplayground/Main3Activity.kt
@@ -2,11 +2,16 @@ package br.com.ricardo.kotlinplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Main3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+
+
+
     }
 }
